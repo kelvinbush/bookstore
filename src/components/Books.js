1cec471eaa19b4feaedfb6f55b725c4a16543390
Kelvin Bush <kelybush@gmail.com>
2022-06-01 21:18:50 +0300

    add prop validation

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import Book from "./Book";
-import AddBook from "./AddBook";
+import Book from './Book';
+import AddBook from './AddBook';
 
 const Books = () => {
   const books = [{
     title: 'The Design of Everyday Things',
     author: 'Don Norman',
+    id: 1,
   },
-    {
-      title: 'The Most Human Human',
-      author: 'Brian Christian',
-    },
-    {
-      title: 'The Design of Everyday Things',
-      author: 'Don Norman',
-    }
-  ]
+  {
+    title: 'The Most Human Human',
+    author: 'Brian Christian',
+    id: 2,
+  },
+  {
+    title: 'The Design of Everyday Things',
+    author: 'Don Norman',
+    id: 3,
+  },
+  ];
   return (
     <div>
-      {books.map(book => <Book title={book.title} author={book.author} />)}
+      {books.map((book) => <Book key={book.id} title={book.title} author={book.author} />)}
       <AddBook />
     </div>
   );
