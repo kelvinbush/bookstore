7a9988e769c10017153577c92816bd5654143b81
Kelvin Bush <kelybush@gmail.com>
2022-06-09 13:01:50 +0300

    fix open and close mobile nav bar

--- src/App.js
+++ src/App.js
@@ -2,49 +2,39 @@
 import { NavLink, Route, Routes } from "react-router-dom";
 import React, { useState } from "react";
 import { IoIosPerson } from "react-icons/io";
-import { FiMenu } from "react-icons/fi";
-import { CgClose } from "react-icons/cg";
+import { CgClose, CgMenu } from "react-icons/cg";
 import Books from "./components/Books";
 import Categories from "./components/Categories";
 
 const App = () => {
-  const [modal, setModal] = useState(false);
+  const [modal, setModal] = useState(true);
 
+  // eslint-disable-next-line no-unused-vars
   const toggleModal = () => {
     setModal(!modal);
   };
 
   return (
     <div>
-      <header>
+      <header className={`${modal ? "" : "close_nav"}`}>
         <h1>Bookstore CMS</h1>
-        {modal ? (
-          <div className="nav">
-            <nav>
-              <NavLink
-                onClick={() => toggleModal()}
-                className="nav__link"
-                to="/"
-              >
-                BOOKS
-              </NavLink>
-              <NavLink
-                onClick={() => toggleModal()}
-                className="nav__link"
-                to="/categories"
-              >
-                CATEGORIES
-              </NavLink>
-            </nav>
-            <IoIosPerson
+        <CgMenu onClick={() => toggleModal()} className="nav__menu" />
+        <div className="nav">
+          <nav className="nav__links">
+            <NavLink onClick={() => toggleModal()} className="nav__link" to="/">
+              BOOKS
+            </NavLink>
+            <NavLink
               onClick={() => toggleModal()}
-              className="nav__person"
-            />
-            <CgClose onClick={() => toggleModal()} className="close" />
-          </div>
-        ) : (
-          <FiMenu onClick={() => toggleModal()} className="nav__menu" />
-        )}
+              className="nav__link"
+              to="/categories"
+            >
+              CATEGORIES
+            </NavLink>
+          </nav>
+          <IoIosPerson onClick={() => toggleModal()} className="nav__person" />
+          <CgClose onClick={() => toggleModal()} className="close" />
+        </div>
       </header>
       <Routes>
         <Route path="/" element={<Books />} />
